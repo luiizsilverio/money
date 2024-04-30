91141ec5f2b2db8efcee1888975c141ceb747f7d
luiizsilverio <luiiz.silverio@gmail.com>
2024-04-29 21:05:35 -0300

    first commit

---
+++ money-mobile/App.js
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+import Home from './src/screens/home/home';
+
+export default function App() {
+  return (
+    <>
+      <Home />
+      {/* <StatusBar style="auto" /> */}
+    </>
+  );
+}
---
+++ money-mobile/src/constants/theme.js
@@ -0,0 +1,18 @@
+const COLORS = {
+  blue: "#3869FF",
+  dark_gray: "#43434A",
+  medium_gray: "#A8A8AB",
+  gray3: "#F3F3F3",
+  white: "#FFFFFF"
+}
+
+const FONT_SIZE = {
+  xs: 11,
+  sm: 14,
+  md: 16,
+  lg: 22,
+  xl: 32,
+  xxl: 64
+}
+
+export { COLORS, FONT_SIZE }
